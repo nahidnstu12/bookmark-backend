9f3cb4e647cd5ba2871d30a98de6a2987ceccb3a
nahid_dev <mazharul.islam.nahid@softbdltd.com>
2023-08-31 18:14:52 +0600

    feat: swagger json test

--- index.ts
+++ index.ts
@@ -1,13 +1,45 @@
 import express, { Request, Response } from "express";
 import AppDataSource from "./database";
-import applyMiddleware from "./middleware/initial";
+// import applyMiddleware from "./middleware/initial";
 import bookRoute from "./route/bookRoute";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 AppDataSource.initialize()
   .then(async () => {
     const app = express();
 
-    applyMiddleware(app);
+    const options = {
+      definition: {
+        openapi: "3.1.0",
+        info: {
+          title: "LogRocket Express API with Swagger",
+          version: "0.1.0",
+          description:
+            "This is a simple CRUD API application made with Express and documented with Swagger",
+          license: {
+            name: "MIT",
+            url: "https://spdx.org/licenses/MIT.html",
+          },
+          contact: {
+            name: "LogRocket",
+            url: "https://logrocket.com",
+            email: "info@email.com",
+          },
+        },
+        servers: [
+          {
+            url: "http://localhost:6050",
+          },
+        ],
+      },
+      apis: ["./routes/*.js"],
+    };
+
+    const specs = swaggerJsdoc(options);
+    app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+    // applyMiddleware(app);
 
     // ROUTES
     app.use("/api/books", bookRoute);
--- middleware/initial.ts
+++ middleware/initial.ts
@@ -1,23 +1,23 @@
 import express from "express";
 import morgan from "morgan";
-// import swaggerUI from "swagger-ui-express";
-// import YAML from "yamljs";
+import swaggerUI from "swagger-ui-express";
+import YAML from "yamljs";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-// import { middleware } from "express-openapi-validator";
-// import fs from "fs";
-// import path from "path";
+import { middleware } from "express-openapi-validator";
+import fs from "fs";
+import path from "path";
 
-// const swaggerFilePath = path.join(__dirname, "../swagger.yaml");
+const swaggerFilePath = path.join(__dirname, "../swagger.yaml");
 
-// const file = fs.readFileSync(swaggerFilePath, "utf8");
-// const swaggerDoc = YAML.load(file);
-//
-// const options = {
-//   swaggerOptions: {
-//     validatorUrl: null,
-//   },
-// };
+const file = fs.readFileSync(swaggerFilePath, "utf8");
+const swaggerDoc = YAML.load(file);
+
+const options = {
+  swaggerOptions: {
+    validatorUrl: null,
+  },
+};
 
 const applyMiddleware = (app: any) => {
   app.use(express.json());
@@ -26,14 +26,14 @@ const applyMiddleware = (app: any) => {
   }
   app.use(cookieParser());
   app.use(cors());
-  // app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc, options));
-  // app.use(
-  //   middleware({
-  //     apiSpec: swaggerFilePath,
-  //     validateResponses: true,
-  //     validateRequests: true,
-  //   }),
-  // );
+  app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc, options));
+  app.use(
+    middleware({
+      apiSpec: swaggerFilePath,
+      validateResponses: true,
+      validateRequests: true,
+    }),
+  );
 };
 
 export default applyMiddleware;
